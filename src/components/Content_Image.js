d176c72565cc101c66028b202310a4ea14aa8a79
DeveloperRyou <DeveloperRyou@gmail.com>
2022-05-22 15:55:29 +0900

    edit components

--- src/components/Content_Image.js
+++ src/components/Content_Image.js
@@ -2,10 +2,10 @@ import './Components.css';
 
 function Content_Image(props) {
   return (
-    <div className="mb-5 row text-left">
+    <div className={props.data.className+" mb-5 row text-left"} >
       <div className='col-lg-7 col-12'>
        <h1 className='d-flex mb-3' dangerouslySetInnerHTML={{__html: props.data.title}} ></h1>
-       <p className='common-p mb-0' dangerouslySetInnerHTML={{__html: props.data.content}}></p>
+       <div dangerouslySetInnerHTML={{__html: props.data.content}}></div>
       </div>
       <div className='my-lg-auto col-lg-4 col-12 offset-lg-1'>
         <img className='img-fluid w-100' src='' onError={(e) => {e.target.src = '/alt.jpg'}}></img>
--- src/data/components_data.js
+++ src/data/components_data.js
@@ -1,9 +1,32 @@
 const introduce = {
-	title : "WELCOME TO THE<br/>BORED APE YACHT CLUB",
-	content : "BAYC is a collection of 10,000 Bored Ape NFTs—unique digital collectibles living on the Ethereum blockchain. Your Bored Ape doubles as your Yacht Club membership card, and grants access to members-only benefits, the first of which is access to THE BATHROOM, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation."
+	className : "introduce",
+	title : "WELCOME TO THE<br/>STUPID CAT CHARITY CLUB",
+	content : "<p class='common-p'>\
+	SC3 is an NFT charity project that satirizes the problem of simply helping others by donating money \
+	without knowing the source of use. \
+	SC3 was created for the purpose of using transparency and proper donations to solve these problems, \
+	not the foundation profits from NFT sales. \
+	SC3 NFT presents solutions to various problems such as unstable persistence, cloning, impersonation, and fraud, \
+	and aims to scale in the right direction.\
+	</p>\
+	<p class='common-p'>\
+	As the use of opaque donations expands, distrust of donations is growing in modern society. \
+	However, the SC3 Foundation aims to become a non-profit organization that grows \
+	with users by capturing these anxiety factors. \
+	When the target amount due to NFT sales is achieved, \
+	it will support unmarried mothers and children's shelters, skipping meals, and teenagers \
+	who cannot purchase health and hygiene products due to lack of financial support.\
+	</p>\
+	<p class='common-p'>\
+	The SC3 Foundation will select users and deliver them directly to relieve anxiety by disclosing where to use, \
+	disclosing the amount, and transparency, and will continue to carry out charity activities \
+	in which the foundation and users participate.\
+	</p>\
+	"
 }
 
 const team = {
+	className : "team",
 	title : "THE TEAM",
 	content : '<p class="common-p">BAYC was created by four friends who set out to make some dope apes, test our skills, and try to build something (ridiculous).</p><p class="common-p"><span class="bayc-color bold-text">GARGAMEL.</span> <span class="font-italic">STARCRAFT OBSESSED. EATS SMURFS.</span></p><p class="common-p"><span class="bayc-color bold-text">GORDON GONER.</span> <span class="font-italic">REFORMED LEVERAGE ADDICT.</span></p><p class="common-p"><span class="bayc-color bold-text">EMPEROR TOMATO KETCHUP.</span> <span class="font-italic">SPENT ALL THEIR MONEY ON FIRST PRESSES AND PET-NAT.</span></p><p class="common-p"><span class="bayc-color bold-text">NO SASS.</span> <span class="font-italic"> HERE FOR THE APES. NOT FOR THE SASS.</span></p>'
 }
